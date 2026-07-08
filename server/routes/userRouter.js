c84f0fb47353fbe39c346bacb02f79df8e4500c2
avigail <avigail3673@gmail.com>
2026-07-08 17:39:04 +0300

    shdrug mexikat mofah

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// מייבאים את קונטרולר המשתמשים
 const userController = require('../controller/userController');
+const { checkAuth } = require('../middleware/checkA.js'); // מייבאים את checkAuth
 
-// נתיב להרשמה: POST /users/signup
+// נתיב להרשמה: POST /users/signup (פתוח לכולם)
 router.post('/users/signup', userController.registerUser);
 
-// נתיב להתחברות: POST /users/login
+// נתיב להתחברות: POST /users/login (פתוח לכולם)
 router.post('/users/login', userController.loginUser);
 
-// נתיב לקבלת פרופיל (ויתרת ארנק) לפי ה-ID שלו: GET /users/:id
-router.get('/users/:id', userController.getUserProfile);
+// נתיב לקבלת פרופיל (ויתרת ארנק): מוגן! רק משתמש מחובר יכול לראות פרופיל
+router.get('/users/:id', checkAuth, userController.getUserProfile);
 
 module.exports = router;
